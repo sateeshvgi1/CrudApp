0b15395c12e0a0e643079d196710975d5e589017
Sateesh Kumar <sateeshvgi@gmail.com>
2026-06-02 09:57:28 +0530

    Api url upadted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        // target: 'http://localhost:5001',
+        target: 'https://crudapp-production-104c.up.railway.app',
         changeOrigin: true,
       },
     },
